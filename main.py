18c81e5de1bdfbb34865cae1882baf86da440b15
Artyom S. <168492973+Art2om@users.noreply.github.com>
2025-03-30 23:49:38 -0700

    Reworked main.py

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 import network
 import socket
+import time
 from website import *
 from temperature import temperature
 from oxymeter import Pulse_Ox
-from distance import measure_distance
+from distance import person_detected
 
 # Encrypt this in the actual final product.
 ssid = 'Local_Network'
 password = 'Local Only'
+
+def generate_html():
+
+    temperature = temperature()
+
+    try:
+        oxygination = Pulse_Ox()
+    except:
+        oxygination = "Error"
+
+    user_active = person_detected()
+
+    # Format the current time to be displayed.
+    current_time = time.localtime()
+    time_string = "{:02d}:{02d}:{02d}".format(current_time[3], current_time[4], current_time[5])
+
+    # Create the HTML with the sensor data.
+    HTML = f"""
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <title>Patient Status:</title>
+            <meta http-equiv = "refresh" content = "10">
+        </head>
+        
+        <body>
+            <h1>Local Patient Status:</h1>
+            <p>Last Updated: {time_sting}</p>
+            <ul>
+                <li>Temperature: {temperature:.1f} °C</li>
+                <li>SpO2 Level: {oxygination}</li>
+                <li>Patient Present: {user_active}</li>
+            </ul>
+        </body>
+    </html>
+    """
+
+    return (HTML)
+
 
 def application():
 
@@ -39,8 +80,12 @@ def application():
             request = str(request)
             print("User Request: " + request)
 
+           response = generate_html()
+
             connection.send("HTTP/1.1 200\n")
-            connection.send(website)
+            connection.send("Conent-Type: text / html\n")
+            connection.send("Closing the Connection.\n\n")
+            connection.send(response)
 
             connection.close()
 
