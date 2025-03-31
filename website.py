6173e3c6c074a3f9423b76c83cfea2011be234cc
Artyom S. <168492973+Art2om@users.noreply.github.com>
2025-03-30 23:54:23 -0700

    Update and rename website.html to website.py

---
+++ website.py
@@ -0,0 +1,16 @@
+# Displayed when sensor data is not gathered.
+
+website = """
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <title>Patient Status</title>
+        </head>
+
+        <body>
+            <h1>Local Patient Status:</h1>
+            <p>Loading sensor data...</p>
+        </body>
+    </html>
+"""
